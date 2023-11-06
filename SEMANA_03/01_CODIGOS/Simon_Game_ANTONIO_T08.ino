eb2cb1f8c8f18b5718019b4dd20b9be64c77bee1
Antonio <123978882+AntonioArtimonte@users.noreply.github.com>
2023-11-06 13:59:50 -0300

    Update Simon_Game_ANTONIO_T08.ino + comentários

--- SEMANA_03/01_CODIGOS/Simon_Game_ANTONIO_T08.ino
+++ SEMANA_03/01_CODIGOS/Simon_Game_ANTONIO_T08.ino
@@ -1,26 +1,33 @@
 
+// Declarar variáveis
+const int nivel_maximo  = 20; // Varíavel nivel máximo ( QUANTO MAIOR, MAIS NÍVEIS )
+int sequence[nivel_maximo]; // Array sequência dos leds
+int your_sequence[nivel_maximo]; // Array sequência dos botões
+int fase  = 1; // Váriavel nivel atual
 
-const int MAX_LEVEL  = 100;
-int sequence[MAX_LEVEL];
-int your_sequence[MAX_LEVEL];
-int level  = 1;
+int dificuldade = 1000; // Váriavel delay entre "piscadas" dos leds
 
-int velocity = 1000;
+
 
 void setup() {
-Serial.begin(9600);
+Serial.begin(9600); // Iniciar com o valor 9600 para o SM
+
+// Declarar pinos dos botões
 pinMode(23, INPUT_PULLUP);
 pinMode(4,  INPUT_PULLUP);
 pinMode(15, INPUT_PULLUP);
 pinMode(13, INPUT_PULLUP);
 
+// Pino botão reset
 pinMode(22, INPUT_PULLUP);
 
+// Declarar pinos dos LEDs
 pinMode(16, OUTPUT);
 pinMode(17,  OUTPUT);
 pinMode(18, OUTPUT);
 pinMode(19, OUTPUT);
 
+// Desligar todos os LEDs
 digitalWrite(16, LOW);
 digitalWrite(17,  LOW);
 digitalWrite(18, LOW);
@@ -31,139 +38,141 @@ digitalWrite(19, LOW);
 void loop()
 {
 
-
-if(digitalRead(22) == LOW && level == 1){
-  show_sequence();
-  get_sequence();
+// Iniciar o jogo com o botão de reset/início
+if(digitalRead(22) == LOW && fase == 1){
+  mostrar_sequencia(); // Chamar a função de mostrar as sequências com os leds
+  pegar_sequencia(); // Chamar a função de pegar a sequência do usuário
   
 }
 
-if  (level == 1)
-generate_sequence();//generate a sequence;
+if  (fase == 1)
+gerar_sequencia(); // Gerar a sequência caso a fase seja 1
 
-if (level != 1) //If start button is pressed or you're winning
+if (fase != 1) 
 {
-show_sequence();    //show the sequence
-get_sequence();     //wait for your sequence
+mostrar_sequencia();  // Ao mudar de fase, mostrar as novas sequencias e adquirir as sequências do usuário
+pegar_sequencia();     
 }
 }
 
-void  show_sequence()
+void  mostrar_sequencia() // Função de mostrar sequência
 {
+
+// Desligar os LEDs
 digitalWrite(16, LOW);
 digitalWrite(17,  LOW);
 digitalWrite(18, LOW);
 digitalWrite(19, LOW);
 
 
-
-for (int i = 0; i < level; i++)
+// Iterar entre os valores da sequência, sendo sempre menor ao número relacionado da fase
+for (int i = 0; i < fase; i++)
 {
-digitalWrite(sequence[i],  HIGH);
-delay(velocity);
-digitalWrite(sequence[i], LOW);
+digitalWrite(sequence[i],  HIGH); // Acender o LED
+delay(dificuldade); // Delay relacionado a fase do usuário
+digitalWrite(sequence[i], LOW); // Desligar o LED
 delay(200);
 }
 }
 
-void  get_sequence()
+void  pegar_sequencia() // Função para pegar a sequência do usuário
 {
-int flag = 0; //this flag indicates if the sequence is correct
+int flag = 0; // Varíavel para checar se a sequencia está correta
 
 
-for  (int i = 0; i < level; i++)
+for  (int i = 0; i < fase; i++) // Iterar para todos os valores de I da sequência relacionada a fase
 {
 flag = 0;
-while(flag == 0)
+while(flag == 0) // Enquanto a sequência estiver correta e a quantidade de itens na array do usuário ser menor que o número de fases, executar código abaixo
 {
-if (digitalRead(23)  == LOW)
+if (digitalRead(23)  == LOW) // Se o botão relacionado ao LED azul ser pressionado, executará o código abaixo
 {
-digitalWrite(16, HIGH);
-your_sequence[i] = 16;
-flag = 1;
+digitalWrite(16, HIGH); // Ligar o LED azul
+your_sequence[i] = 16; // Adicionar a array dos botões o botão 16 = AZUL
+flag = 1; // Mudar para 1 a flag, mostrando que um botão foi adicionado a array
 delay(200);
-if  (your_sequence[i] != sequence[i])
+if  (your_sequence[i] != sequence[i]) // Checar a array do usuário e dos LEDs para checar se estão corretas
 {
-wrong_sequence();
+sequencia_errada(); // Caso estejam erradas as arrays, chamar a função "sequência errada"
 return;
 }
 
-digitalWrite(16,  LOW);
+digitalWrite(16,  LOW); // Desligar o LED azul
 }
 
-if (digitalRead(4) == LOW)
+if (digitalRead(4) == LOW) // Se o botão relacionado ao LED verde ser pressionado, executará o código abaixo
 {
-digitalWrite(17, HIGH);
-your_sequence[i]  = 17;
-flag = 1;
+digitalWrite(17, HIGH); // Ligar o LED verde
+your_sequence[i]  = 17; // Adicionar a array dos botões o botão 17 = VERDE
+flag = 1; // Mudar para 1 a flag, mostrando que um botão foi adicionado a array
 delay(200);
-if (your_sequence[i] != sequence[i])
+if (your_sequence[i] != sequence[i])  // Checar a array do usuário e dos LEDs para checar se estão corretas
 {
-wrong_sequence();
+sequencia_errada(); // Caso estejam erradas as arrays, chamar a função "sequência errada"
 return;
 }
-digitalWrite(17,  LOW);
+digitalWrite(17,  LOW); // Desligar o LED azul
 }
 
-if (digitalRead(15) == LOW)
+if (digitalRead(15) == LOW) // Se o botão relacionado ao LED amarelo ser pressionado, executará o código abaixo
 {
-digitalWrite(18, HIGH);
-your_sequence[i]  = 18;
-flag = 1;
+digitalWrite(18, HIGH); // Ligar o LED amarelo
+your_sequence[i]  = 18; // Adicionar a array dos botões o botão 15 = AMARELO
+flag = 1; // Mudar para 1 a flag, mostrando que um botão foi adicionado a array
 delay(200);
-if (your_sequence[i] != sequence[i])
+if (your_sequence[i] != sequence[i]) // Checar a array do usuário e dos LEDs para checar se estão corretas
 {
-wrong_sequence();
+sequencia_errada(); // Caso estejam erradas as arrays, chamar a função "sequência errada"
 return;
 }
-digitalWrite(18,  LOW);
+digitalWrite(18,  LOW); // Desligar o LED amarelo
 }
 
-if (digitalRead(13) == LOW)
+if (digitalRead(13) == LOW) // Se o botão relacionado ao LED vermelho ser pressionado, executará o código abaixo
 {
-digitalWrite(19, HIGH);
-your_sequence[i]  = 19;
-flag = 1;
+digitalWrite(19, HIGH); // Ligar o LED vermelho
+your_sequence[i]  = 19; // Adicionar a array dos botões o botão 13 = VERMELHO
+flag = 1; // Mudar para 1 a flag, mostrando que um botão foi adicionado a array
 delay(200);
-if (your_sequence[i] != sequence[i])
+if (your_sequence[i] != sequence[i]) // Checar a array do usuário e dos LEDs para checar se estão corretas
 {
-wrong_sequence();
+sequencia_errada(); // Caso estejam erradas as arrays, chamar a função "sequência errada"
 return;
 }
-digitalWrite(19,  LOW);
+digitalWrite(19,  LOW); // Desligar o LED vermelho
 }
 
-if (digitalRead(22) == LOW)
+if (digitalRead(22) == LOW) // Se o botão de reset for pressionado, executará o código abaixo
 {
-your_sequence[i]  = 22;
-flag = 1;
+your_sequence[i]  = 22; // Adicionar o botão de reset, 22 a array de botões
+flag = 1; // Adicionar a flag afim de evitar erros
 delay(200);
-if (your_sequence[i] != sequence[i])
+if (your_sequence[i] != sequence[i]) // Como o botão de reset, 22 não tem um LED referenciável, ira retornar a sequencia errada diretamente
 {
-wrong_sequence();
+sequencia_errada(); // Caso estejam erradas as arrays, chamar a função "sequência errada"
 return;
 }
-digitalWrite(30,  LOW);
 }
 
 }
 }
-right_sequence();
+sequencia_correta(); // Caso a sequência seja correta, chamar a função "sequencia_correta"
 }
 
-void generate_sequence()
+void gerar_sequencia() // Função para gerar a sequencia
 {
-randomSeed(millis());  //in this way is really random!!!
+randomSeed(millis());  // Função de randomizar
 
-for (int i = 0; i < MAX_LEVEL; i++)
+for (int i = 0; i < nivel_maximo; i++) // Gerar a sequência de 0 até o nivel máximo dito na váriavel no começo
 {
-sequence[i]  = random(16,20);
+sequence[i]  = random(16,20); // Adicionar a array da sequência dos LEDs números randomizados entre 16 e 19 que são os respectivos pinos dos LEDs
 }
 }
-void wrong_sequence()
+void sequencia_errada() // Função caso a sequência seja errada
 {
-for (int i = 0; i < 3;  i++)
+for (int i = 0; i < 3;  i++) // Irá iterar na quantidade de LEDs
 {
+// Acenderá e apagará todos os LEDs
 digitalWrite(16, HIGH);
 digitalWrite(17,  HIGH);
 digitalWrite(18, HIGH);
@@ -175,12 +184,15 @@ digitalWrite(18, LOW);
 digitalWrite(19, LOW);
 delay(250);
 }
-level  = 1;
-velocity = 1000;
+// Voltará a fase para 1 
+fase  = 1;
+// Voltará a dificuldade para 1000
+dificuldade = 1000;
 }
 
-void right_sequence()
+void sequencia_correta() // Função caso a sequência seja correta
 {
+// Apagar, acender e apagar todos os LEDs
 digitalWrite(16, LOW);
 digitalWrite(17,  LOW);
 digitalWrite(18, LOW);
@@ -198,8 +210,8 @@ digitalWrite(18, LOW);
 digitalWrite(19, LOW);
 delay(500);
 
-if  (level < MAX_LEVEL);
-level++;
+if  (fase < nivel_maximo); // Caso a fase seja menor que o nivel maximo, irá auemntar a fase em 1 e, "aumentar" a dificuldade em 50
+fase++;
 
-velocity -= 50; //increase difficulty
+dificuldade -= 50;
 }
